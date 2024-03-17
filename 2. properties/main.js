b74d0ed6c1c82eacb0fb62d7cad2377a8ce56465
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-03-17 11:54:59 +0530

    init

--- 2. properties/main.js
+++ 2. properties/main.js
@@ -5,10 +5,20 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+
+    // frame:false , 
+    // backgroundColor:'#ff0000',
+    // alwaysOnTop:true,
+    // title:'New Title',
+    resizable:false,
   })
 
   // Load index.html into the window
   win.loadFile('index.html')
+
+  // Dev Tool
+  win.webContents.openDevTools();
+
 }
 
 // When Electron has finished initializing and is ready to create browser windows.
